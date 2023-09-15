1992de1280be990b6916c046af61d57bbae603ea
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-09-15 12:05:40 +0100

    Update HelloController.java

--- src/main/java/com/demonstration/web/HelloController.java
+++ src/main/java/com/demonstration/web/HelloController.java
@@ -1,12 +1,16 @@
 package com.demonstration.web;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 @GetMapping
-public String hello()
+
+public String hello(@PathVariable long id)
 {
 return "hello security";	
 }
